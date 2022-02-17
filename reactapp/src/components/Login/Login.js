71569381a193cd2c3ddfd6f026065b15f85c91a8
Keshav Ojha <keshavojha98@gmail.com>
2022-02-17 13:48:16 +0530

    Update Login.js

--- reactapp/src/components/Login/Login.js
+++ reactapp/src/components/Login/Login.js
@@ -12,7 +12,7 @@ export const Login = () => {
       .email('Email is invalid')
       .required('Email is required'),
     password: Yup.string()
-      .min(6, 'Password must be at least 6 charaters')
+      .min(6, 'Password must be at least 6 characters')
       .required('Password is required'),
   });
 
