1e8d0b1999d638a6b193ad3dc6933f8d39347dd4
renukadevibolla <78066548+renukadevibolla@users.noreply.github.com>
2022-02-17 21:16:40 +0530

    landing page

--- reactapp/src/pages/LandingPage/LandingPage.js
+++ reactapp/src/pages/LandingPage/LandingPage.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import classes from './LandingPage.module.css';
+
 const LandingPage = () => {
+
   return (
-      <div className={classes.container}>
-        <div className={classes.element}>
-          <h1>Welcome to Vaccum Cleaner Service App</h1>
+    <div className={classes.container}>
+      <div className={classes.image}></div>
+        <div className={classes.element} >
+          <h2>Welcome to Vaccum Cleaner Service App</h2>
+          <p>Now book your vaccum cleaner service at your finger tips.</p>
+          <p>Get Started !</p>
+          <Link className="btn btn-primary" style={{marginRight:"16px"}}to="/signup">Signup</Link>
+          <h3>Already have an account?</h3>
+          <Link className="btn btn-success" to="/login">Login</Link>
+          </div>
         </div>
-        <div className={classes.element}>
-          <p style={{textAlign:'center',fontSize:'20px'}}>Now book your vaccum cleaner service at your finger tips.</p>
-          <p style={{textAlign:'center',fontSize:'20px'}}>Get Started !</p>
-        </div>
-        <div className={classes.element}>
-          <Link className="btn btn-dark" style={{marginRight:"16px"}}to="/signup">Signup</Link>
-          <Link className="btn btn-light" to="/login">Login</Link>
-        </div>
+       
         
-      </div>
+  
+        
+       
+       
+  
+        
+       
+        
+      
   );
 };
 
