8a2e69d63c6380aabfd642100fbd0608dc6e8a97
Senthilvasudevan <vbsukl2000@gmail.com>
2022-02-27 15:14:44 +0530

    AppointmentInfo_completed

--- springapp/src/main/java/com/examly/springapp/repo/CenterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.examly.springapp.repo;
-
-import com.examly.springapp.model.Center;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CenterRepository extends JpaRepository<Center, Long> {
-}
