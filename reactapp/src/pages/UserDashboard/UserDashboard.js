c94e0a605ce11642653962be10b58e01f19711c7
Keshav Ojha <keshavojha98@gmail.com>
2022-02-27 21:43:13 +0530

    A lot of changes made

--- reactapp/src/pages/UserDashboard/UserDashboard.js
+++ reactapp/src/pages/UserDashboard/UserDashboard.js
@@ -1,12 +1,24 @@
-import React from 'react'
-import UserDashBoard from '../../components/UserDashBoard/UserDashBoard'
+import React, { useEffect, useState } from 'react'
+import BookAppointmentForm from '../../components/BookAppointmentForm/BookAppointmentForm'
+import styles from './UserDashboard.module.css';
+import UserCentreCard from '../../components/UserCentreCard/UserCentreCard';
+function UserDashboard(props) {
+  const {cardData,setCardData} = useState({});
 
-function UserDashboard() {
+  useEffect(()=>{
+    const card = JSON.parse(localStorage.get("bookCenterDetails"));
+    setCardData(card);
+  })
+
   return (
-      <div>
-        
-            <UserDashBoard/>
-              </div>
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <UserCentreCard data={cardData}/>
+      </div>
+      <div className={styles.form}>
+        <BookAppointmentForm center={cardData}/>
+      </div>
+    </div>
   )
 }
 
