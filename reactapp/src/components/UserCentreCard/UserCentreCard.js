815f48ae1184213a4df3029f547f943586371c18
Anuhya0927 <srilumutyala123@gmail.com>
2022-02-27 21:36:07 +0530

    UserCenterCard

--- reactapp/src/components/UserCentreCard/UserCentreCard.js
+++ reactapp/src/components/UserCentreCard/UserCentreCard.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import styles from './UserCentreCard.module.css';
+import AddIcon from '@material-ui/icons/Add';
+import {Link} from 'react-router-dom';
+function UserCentreCard(props) {   
+    const handleOnClickAdd=()=>{
+        localStorage.setItem("data",JSON.stringify(props.data));
+    }
 
-function UserCentreCard(props) {
   return (
     <div className={`container, ${styles.main}`} style={props.style}>
         <div className="row">
@@ -15,7 +20,14 @@ function UserCentreCard(props) {
                         <p class="card-text">{`Phone: ${props.data.mobileNumber}`}</p>
                         <p class="card-text">{`Email: ${props.data.email}`}</p>
                         <p style={{fontSize:'18px',color:'green'}} class="card-text">{props.data.description}</p>
-                        
+                        <>
+                        {
+                                <Link to="/dashboard" style={{marginRight:'8px'}} onClick={()=>handleOnClickAdd()}
+                                >
+                                <AddIcon/>
+                                </Link>
+                            }
+                        </>
                     </div>
                 </div>
             </div>
