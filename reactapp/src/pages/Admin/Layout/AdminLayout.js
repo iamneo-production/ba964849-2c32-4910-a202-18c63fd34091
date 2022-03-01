e809bd3668c586c188a59ac6786cf7cee490402d
Keshav Ojha <keshavojha98@gmail.com>
2022-03-02 00:50:45 +0530

    Revert "Login Logout full feature implemented"

--- reactapp/src/pages/Admin/Layout/AdminLayout.js
+++ reactapp/src/pages/Admin/Layout/AdminLayout.js
@@ -1,24 +1,13 @@
-import React, { useEffect } from 'react'
-import { Outlet, useNavigate } from 'react-router-dom'
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from 'react'
+import { Outlet } from 'react-router-dom'
 import AdminNavbar from '../../../components/Admin/AdminNavbar/AdminNavbar'
 
 function AdminLayout() {
-  const user = JSON.parse(localStorage.getItem("user"));
-  const navigate = useNavigate();
-  useEffect(()=>{
-    if(user==null || user.data.userType !== "ADMIN"){
-      toast.error("Please Login to see this page");
-      navigate("/login");
-    }
-  })
   return (
     <div>
         <AdminNavbar/>
         <h1>Welcome, Admin</h1>
         <br></br>
-        <ToastContainer/>
         <Outlet/>
     </div>
   )
--- reactapp/src/pages/User/Layout/UserLayOut.js
+++ reactapp/src/pages/User/Layout/UserLayOut.js
@@ -1,36 +1,17 @@
-import React, { useEffect } from 'react'
-import { Outlet, useNavigate } from 'react-router-dom'
+import React from 'react'
+import { Outlet } from 'react-router-dom'
 import UserNavbar from '../../../components/User/UserNavbar/UserNavbar'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 function UserLayOut() {
-
-  const navigate = useNavigate();
-
   const user = JSON.parse(localStorage.getItem("user"));
   console.log("userrrrrrrrrrr",user);
-
-  useEffect(()=>{
-    if(user==null || user.data.userType!=="USER"){
-      toast.error('Please Login to view this page');
-      navigate("/login");
-    }
-  },[])
-
   return (
-    <>
-      {
-      user && 
-      <div>
+    <div>
         <UserNavbar/>
         <h1>Welcome, {user.data.name}</h1>
         <br></br>
-        <ToastContainer/>
         <Outlet/>
-      </div>
-    }
-    </>
+    </div>
   )
 }
 
