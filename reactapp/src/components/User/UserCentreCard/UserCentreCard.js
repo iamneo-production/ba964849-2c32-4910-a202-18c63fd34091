1bedc8010b9345c4113ffd69b01d97a765736ccb
Keshav Ojha <keshavojha98@gmail.com>
2022-03-05 20:40:34 +0530

    Design changes

--- reactapp/src/components/User/UserCentreCard/UserCentreCard.js
+++ reactapp/src/components/User/UserCentreCard/UserCentreCard.js
@@ -14,29 +14,23 @@ function UserCentreCard(props) {
     }
 
   return (
-    <div className={`container, ${styles.main}`} style={props.style}>
-        <div className="row">
-            <div className="col-sm">
-                <div class="card" >
-                    <img style={{width:'100%',height:'250px',objectFit:'fill'}}src={props.data.imgUrl} class="card-img-top" id="Grid1" alt="Vacuumservice"/>
-                    <div class="card-body">
-                        <h5 class="card-title">{props.data.name}</h5>
-                        <p class="card-text"><LocationOnSharpIcon style={{fontSize:"small",color:"black",marginRight:"4px"}}/>{` ${props.data.address}, ${props.data.city}-${props.data.pincode}`}</p>
-                        <p class="card-text"><AccessTimeFilledSharpIcon style={{fontSize:"small",color:"black",marginRight:"4px"}}/> 10:00AM to 05:00PM</p>
-                        <p class="card-text"><PhoneAndroidSharpIcon style={{fontSize:"small",color:"black",marginRight:"4px"}}/>{` ${props.data.mobileNumber}`}</p>
-                        <p class="card-text"><EmailSharpIcon style={{fontSize:"small",color:"black",marginRight:"4px"}}/>{` ${props.data.email}`}</p>
-                        <p style={{fontSize:'18px',color:'darkgray'}} class="card-text">{props.data.description}</p>
-                        <>
-                        {   props.enableOptions &&
-                            <Link to="/user/dashboard" style={{marginRight:'8px'}} onClick={()=>handleOnClickAdd()}
-                            >
-                            <span className={`btn-success ${styles.bookButton}`}>Book<AddIcon/></span>
-                            </Link>
-                        }
-                        </>
-                    </div>
-                </div>
-            </div>
+    <div className={`card ${styles.card}`} >
+        <img style={{width:'100%',height:'250px',objectFit:'fill'}}src={props.data.imgUrl} class="card-img-top" id="Grid1" alt="Vacuumservice"/>
+        <div class="card-body">
+            <h5 class="card-title">{props.data.name}</h5>
+            <p class="card-text"><LocationOnSharpIcon style={{fontSize:"small",color:"black",marginRight:"4px"}}/>{` ${props.data.address}, ${props.data.city}-${props.data.pincode}`}</p>
+            <p class="card-text"><AccessTimeFilledSharpIcon style={{fontSize:"small",color:"black",marginRight:"4px"}}/> 10:00AM to 05:00PM</p>
+            <p class="card-text"><PhoneAndroidSharpIcon style={{fontSize:"small",color:"black",marginRight:"4px"}}/>{` ${props.data.mobileNumber}`}</p>
+            <p class="card-text"><EmailSharpIcon style={{fontSize:"small",color:"black",marginRight:"4px"}}/>{` ${props.data.email}`}</p>
+            <p style={{fontSize:'18px',color:'darkgray'}} class="card-text">{props.data.description}</p>
+            <>
+            {   props.enableOptions &&
+                <Link to="/user/dashboard" style={{marginRight:'8px'}} onClick={()=>handleOnClickAdd()}
+                >
+                <span className={`btn-success ${styles.bookButton}`}>Book<AddIcon/></span>
+                </Link>
+            }
+            </>
         </div>
     </div>
   )
