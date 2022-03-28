1752906e6d59b5f0ba15e90612af668a18140f7e
tharunraj44 <51993216+tharunraj44@users.noreply.github.com>
2022-03-28 22:18:41 +0530

    Update Userreview.js

--- reactapp/src/pages/User/Userreview/Userreview.js
+++ reactapp/src/pages/User/Userreview/Userreview.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import classes from './Userreview.module.css';
 import UserReview from '../../components/UserReview/UserReview'
-import re from '../../assets/re.jpg'
+
 
 
 function Userreview() {
 
   return (
-    
-    
+
     <div className={classes.container}>
       <div className={classes.image}></div>
       <UserReview/>
         </div>   
-  
 
-      
+
+
   )
 }
 
