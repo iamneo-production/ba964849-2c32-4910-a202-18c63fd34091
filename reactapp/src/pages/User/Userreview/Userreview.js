ea6d26746a998b8cbcebb9147b31660d56f0b18e
Keshav Ojha <keshavojha98@gmail.com>
2022-03-30 20:38:33 +0530

    Minor chages to review

--- reactapp/src/pages/User/Userreview/Userreview.js
+++ reactapp/src/pages/User/Userreview/Userreview.js
@@ -54,13 +54,16 @@ function Userreview(props) {
     try{
       const res = await submitReview(data);
       toast.success("Review Submitted");
+      setTimeout(()=>{
+        window.location.reload();
+      },3000);
     }
     catch(error){
       console.log(error);
       toast.error("Review Submit Failed");
     }
 
-    fetchReviews();
+
   }
 
   return(
